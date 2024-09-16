b62b21ab9f8c42fd0d3820af205baf6ad4d59b03
madicoulson <coulsomj@mail.uc.edu>
2024-09-16 15:36:09 -0400

    changed header

--- project-1/src/lib/Header.svelte
+++ project-1/src/lib/Header.svelte
@@ -12,7 +12,7 @@
     $: reactiveDays = $dateDifference;
 </script>
   
-<header>Fitness Journal
+<header>Madi's Fitness Journal
     <p class="dateTime"> {reactiveDays} days since start | {reactiveLength} days active | {currentDateTime}</p>
 </header>
 
@@ -30,7 +30,7 @@ header {
 .dateTime {
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   color: rgb(52, 76, 98);
-  font-size: 30px;
+  font-size: 25px;
   font-weight: 1000;
   height: fit-content;
   padding: 10px;
