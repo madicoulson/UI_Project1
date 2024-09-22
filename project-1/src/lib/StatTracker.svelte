edc9b3307a8309515a60ae90a1378d341ed5117c
madicoulson <coulsomj@mail.uc.edu>
2024-09-22 15:51:39 -0400

    Stat Tracker commenting

--- project-1/src/lib/StatTracker.svelte
+++ project-1/src/lib/StatTracker.svelte
@@ -3,98 +3,136 @@
     import { Chart } from 'chart.js/auto';
     import { onMount } from "svelte";
 
-    // Chart definitions for within Tracking Stats component
+    //Binded global variables for charts
     let durationChart;
     let stepChart;
-    let chart1, chart2;
+    let durationChartCreation;
+    let stepChartCreation;
 
+    // Function to render and display graphs on mount of the page.
     onMount(()=> {
         renderGraphs();
     })
 
+    // Function to create and render graphs.
     function renderGraphs() {
-        // Prepare the labels (dates) and dataset (durations) for the chart
+        // Map the date, duration, and steps
         let dateLabels = dataPool.map(entry => entry.date);
         const durations = dataPool.map(entry => entry.workout.duration);
         const steps = dataPool.map(entry=> entry.steps);
 
+        // Define data for the workout duration graph
         const durationData = {
             labels: dateLabels,
             datasets: [
                 {
                     label: 'Workout Durations',
                     data: durations,
-                    backgroundColor: ['#7000e1', '#fc8800', '#00b0e8'],
+                    backgroundColor: ['#00b0e8'],
                     fill: false
                 }
             ]
         };
 
+        // Define data for the steps graph
         const dataSteps = {
             labels: dateLabels,
             datasets: [
                 {
                     label: 'Step Count',
                     data: steps,
-                    backgroundColor: ['#7000e1', '#fc8800', '#00b0e8'],
+                    backgroundColor: ['#00b0e8'],
                     fill: false
                 }
             ]
         };
 
-        // Destroy existing charts if they exist
-        if (chart1) chart1.destroy();
-        if (chart2) chart2.destroy();
+        // Destroy existing charts if they already exist
+        if (durationChartCreation) durationChartCreation.destroy();
+        if (stepChartCreation) stepChartCreation.destroy();
 
-        const ctx = durationChart.getContext('2d');
-        // Initialize chart using default config set
-        chart1 = new Chart(ctx, {
-        type: 'line',
-        data: durationData,
-        options: {
-            maintainAspectRatio: false,
-            responsive: true,
-            scales: {
-            x: {
-                title: {
-                text: 'Date',
-                display: true
-                }
-            },
-            y : {
-                title: {
-                text: 'Duration in Minutes',
-                display: true
+        // Define the durationChart context
+        const durationContext = durationChart.getContext('2d');
+
+        // Define the chart using the durationData and context
+        durationChartCreation = new Chart(durationContext, {
+            type: 'line',
+            data: durationData,
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                scales: {
+                    x: {
+                        title: {
+                            text: 'Date',
+                            display: true
+                        }
+                    },
+                    y : {
+                        title: {
+                            text: 'Duration in Minutes',
+                            display: true
+                        },
+                        beginAtZero:true
+                    }
                 },
-                beginAtZero:true
+                plugins: {
+                    legend: {
+                        labels: {
+                            font: {
+                                family: 'Verdana',
+                                size: 12,      
+                                style: 'normal',
+                                weight: 'bold',  
+                            }
+                        }
+
+                    },
+
+                }
             }
-            }
-        }
         });
-        const ctx2 = stepChart.getContext('2d');
-        // Initialize chart using default config set
-        chart2 = new Chart(ctx2, {
-        type: 'line',
-        data: dataSteps,
-        options: {
-            maintainAspectRatio: false,
-            responsive: true,
-            scales: {
-            x: {
-                title: {
-                text: 'Date',
-                display: true
-                }
-            },
-            y : {
-                title: {
-                text: 'Step Count',
-                display: true
+
+        // Define the stepChart context
+        const stepContext = stepChart.getContext('2d');
+
+        // Define the chart using the stepData and context
+        stepChartCreation = new Chart(stepContext, {
+            type: 'line',
+            data: dataSteps,
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                scales: {
+                    x: {
+                        title: {
+                            text: 'Date',
+                            display: true
+                        }
+                    },
+                    y : {
+                        title: {
+                            text: 'Step Count',
+                            display: true
+                        },
+                        beginAtZero:true
+                    }
                 },
-                beginAtZero:true
+                plugins: {
+                    legend: {
+                        labels: {
+                            font: {
+                                family: 'Verdana',
+                                size: 12,      
+                                style: 'normal',
+                                weight: 'bold',  
+                            }
+                        }
+
+                    },
+
+                }
             }
-            }
-        }
         });
     }
 </script>
@@ -102,47 +140,56 @@
 
 <statTracker>
     <p class="component_header"> Tracking Stats </p>
+
+    <!-- Button definition to refresh graphs upon data change. -->
     <button class="component_button_top_stats" on:click={()=>renderGraphs()}>Refresh Graphs</button>
     <br> <br> <br>
+    
+    <!-- Canvas binding for duration chart. -->
     <canvas bind:this={durationChart}></canvas>
     <br> <br>
+
+    <!-- Canvas binding for step chart. -->
     <canvas bind:this={stepChart}></canvas>
 </statTracker>
 
 <style>
-statTracker {
-    background-color: rgb(228, 234, 238);
-    height: 50vh;
-    width: 33vw;
-    max-width: 100%;
-    max-height: 100%;
-    border: rgb(52, 76, 98);
-    border-width: 1.5px;
-    border-style: solid;
-    padding: 10px;
-    margin: 10px;
-    position: relative;
-    border-radius: 16px;
-    overflow:auto;
-}
+    /*statTracker component style defintion**/
+    statTracker {
+        background-color: rgb(228, 234, 238);
+        height: 50vh;
+        width: 33vw;
+        max-width: 100%;
+        max-height: 100%;
+        border: rgb(52, 76, 98);
+        border-width: 1.5px;
+        border-style: solid;
+        padding: 10px;
+        margin: 10px;
+        position: relative;
+        border-radius: 16px;
+        overflow:auto;
+    }
 
-.component_button_top_stats {
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  font-size: 14px;
-  padding: 6px 12px 6px 12px;
-  margin-top: 10px;
-  background-color:rgb(52, 76, 98);
-  color: white;
-  border-radius: 16px;
-  width:fit-content;
-  top: 57px;
-  right: 10px;
-  position: absolute;
-}
+    /*Absolute position button style defintion**/
+    .component_button_top_stats {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 14px;
+        padding: 6px 12px 6px 12px;
+        margin-top: 10px;
+        background-color:rgb(52, 76, 98);
+        color: white;
+        border-radius: 16px;
+        width:fit-content;
+        top: 57px;
+        right: 10px;
+        position: absolute;
+    }
 
-canvas {
-  width: 100%;
-  height: 100%;
-  background-color:  rgb(211, 217, 222);
-}
+    /*Canvas style defintion**/
+    canvas {
+        width: 100%;
+        height: 100%;
+        background-color:  rgb(211, 217, 222);
+    }
 </style>
